777ff3a9a066ed66ee40fa645e482e054b6471ce
hanchen <hanchenjhc@gmail.com>
2019-08-17 17:54:09 -0500

    add unstyled LaunchPage

---
+++ src/LaunchPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function LaunchPage() {
+    return (
+        <div>
+            <p>
+                Conscious Living
+            </p>
+            <p>
+                Effortless Sharing
+            </p>
+        </div>
+    );
+}
+
+export default LaunchPage;
