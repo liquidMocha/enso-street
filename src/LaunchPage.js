1dd4a4cbe93e0e8455c0d95787efe4f7279bb13d
Hanchen Jiang <hanchenjhc@gmail.com>
2019-08-18 20:02:44 -0500

    Google signin WIP

--- src/LaunchPage.js
+++ src/LaunchPage.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import './styles/LaunchPage.scss';
+import Button from "./Button";
 
-function LaunchPage() {
-    return (
-        <div className="launch-page">
-            <div id="enso-street-title">
-                <p>Enso</p>
-                <p>Street</p>
+class LaunchPage extends React.Component {
+     onSignIn = (googleUser) => {
+        var profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    };
+
+    render() {
+        return (
+            <div className="launch-page">
+                <div id="enso-street-title">
+                    <p>Enso</p>
+                    <p>Street</p>
+                </div>
+                <Button data-onsuccess={this.onSignIn} onclick={this.signin}>Sign In Through Google</Button>
+                <div>
+                    Conscious Living
+                </div>
+                <div>
+                    Effortless Sharing
+                </div>
             </div>
-            <div>
-                Conscious Living
-            </div>
-            <div>
-                Effortless Sharing
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default LaunchPage;
