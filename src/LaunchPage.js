e43c516383c8aad9f007b3154ba2c28e2db9e366
hanchen <hanchenjhc@gmail.com>
2019-08-20 22:15:26 -0500

    [#167503138] Using react-google-login instead.

--- src/LaunchPage.js
+++ src/LaunchPage.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './styles/LaunchPage.scss';
-import Button from "./Button";
+import GoogleLogin from "react-google-login";
 
 class LaunchPage extends React.Component {
-     onSignIn = (googleUser) => {
-        var profile = googleUser.getBasicProfile();
+
+    onSignInFailure = () => {
+        console.log("sign in failed");
+    };
+
+    onSignIn = (googleUser) => {
+        console.log("onSignIn called from jsx");
+        console.log(googleUser);
+        let profile = googleUser.getBasicProfile();
         console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
         console.log('Name: ' + profile.getName());
         console.log('Image URL: ' + profile.getImageUrl());
@@ -18,7 +25,13 @@ class LaunchPage extends React.Component {
                     <p>Enso</p>
                     <p>Street</p>
                 </div>
-                <Button data-onsuccess={this.onSignIn} class="g-signin2">Sign In Through Google</Button>
+                <GoogleLogin
+                    clientId="600326466228-h28741e5k0gksv3440nnn688rnl967bb.apps.googleusercontent.com"
+                    buttonText="Login"
+                    onSuccess={this.onSignIn}
+                    onFailure={this.onSignInFailure}
+                    cookiePolicy={'single_host_origin'}
+                />
                 <div>
                     Conscious Living
                 </div>
