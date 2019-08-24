6b66f5a6fb455e657a137fbe42f1ac9b6caaac99
hanchen <hanchenjhc@gmail.com>
2019-08-23 23:35:19 -0500

    [finishes #168020330] Add search bar

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './styles/SearchBar.scss';
 import MenuButton from "./MenuButton";
+import SearchBox from "./SearchBox";
 
 class SearchBar extends React.Component {
     render() {
         return (
             <div className="search-bar">
                 <MenuButton/>
+                <SearchBox/>
             </div>
         );
     }
---
+++ src/SearchBox.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './styles/SearchBox.scss'
+
+class SearchBox extends React.Component {
+    render() {
+        return (
+            <div className="search-box-container">
+                <i className="fa fa-search"/>
+                <input type="text" className="search-box"/>
+            </div>
+        );
+    }
+}
+
+export default SearchBox;
