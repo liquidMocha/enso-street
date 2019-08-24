663158235df7cdc62a54955bfb9b25375e06f4c1
hanchen <hanchenjhc@gmail.com>
2019-08-23 23:01:20 -0500

    [finishes #168020331] Add menu button to search bar

---
+++ src/MenuButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './styles/MenuButton.scss';
+
+const MenuButton = () => {
+    return (
+        <div className="menu-button-container">
+            <div className="menu-button-bar"/>
+            <div className="menu-button-bar"/>
+            <div className="menu-button-bar"/>
+        </div>
+    );
+};
+
+export default MenuButton;
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './styles/SearchBar.scss';
+import MenuButton from "./MenuButton";
 
 class SearchBar extends React.Component {
     render() {
         return (
             <div className="search-bar">
-
+                <MenuButton/>
             </div>
         );
     }
