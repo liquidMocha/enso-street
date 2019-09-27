51c7f7664620de7d2ff46f60f4609c57e7b6853d
hanchen <hanchenjhc@gmail.com>
2019-09-27 09:45:09 -0500

    convert SignUpPage into function
    [#168807411] (https://www.pivotaltracker.com/story/show/168807411)

--- src/SignUpPage.js
+++ src/SignUpPage.js
@@ -1,80 +1,65 @@
-import React from "react";
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import './styles/SignUpPage.scss';
 import {Link} from "react-router-dom";
 import * as ReactGA from "react-ga";
 
-class SignUpPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: "",
-            name: "",
-            password: "",
-            signUpFailed: false
-        };
-    }
+function SignUpPage(props) {
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const [signUpFailed, setSignUpFailed] = useState(false);
 
-    componentDidMount() {
-        ReactGA.pageview('/sign-up');
-    }
+    useEffect(() => ReactGA.pageview('/sign-up'));
 
-    onSubmitSignUpForm = (event) => {
+    const onSubmitSignUpForm = (event) => {
         event.preventDefault();
-        if (this.state.password.length < 8) {
-            this.setState({signUpFailed: true});
+        if (password.length < 8) {
+            setSignUpFailed(true);
             return;
         }
-        axios.post(this.props.baseUrl + '/users/createUser', {
-            email: this.state.email,
-            name: this.state.name,
-            password: this.state.password
+        axios.post(props.baseUrl + '/users/createUser', {
+            email: email,
+            name: name,
+            password: password
         });
     };
 
 
-    render() {
-        return (
-            <div className='sign-up-page'>
-                <div>Sign Up</div>
-                <form onSubmit={this.onSubmitSignUpForm}>
-                    <div>
-                        <label>
-                            Email:
-                            <input id='sign-up-email-field'
-                                   type='text'
-                                   onChange={(event) => {
-                                       this.setState({email: event.target.value})
-                                   }}/>
-                        </label>
-                    </div>
-                    <div>
-                        <label>
-                            Name:
-                            <input id='sign-up-name-field'
-                                   type='text'
-                                   onChange={(event) => {
-                                       this.setState({name: event.target.value})
-                                   }}/>
-                        </label>
-                    </div>
-                    <div>
-                        <label>
-                            Password:
-                            <input id='sign-up-password-field'
-                                   type='password'
-                                   onChange={(event) => {
-                                       this.setState({password: event.target.value})
-                                   }}/>
-                        </label>
-                    </div>
-                    <Link to='login' id='sign-up-page-login-link'>Login</Link>
-                    <button id='sign-up-submit-button' onClick={this.onSubmitSignUpForm}>Sign Up</button>
-                </form>
-                {this.state.signUpFailed ? 'sign up failed' : null}
-            </div>
-        );
-    }
+    return (
+        <div className='sign-up-page'>
+            <div>Sign Up</div>
+            <form onSubmit={onSubmitSignUpForm}>
+                <div>
+                    <label>
+                        Email:
+                        <input id='sign-up-email-field'
+                               type='text'
+                               onChange={(event) => {setEmail(event.target.value)}}/>
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Name:
+                        <input id='sign-up-name-field'
+                               type='text'
+                               onChange={(event) => {setName(event.target.value)}}/>
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Password:
+                        <input id='sign-up-password-field'
+                               type='password'
+                               onChange={(event) => {setPassword(event.target.value)}}/>
+                    </label>
+                </div>
+                <Link to='login' id='sign-up-page-login-link'>Login</Link>
+                <button id='sign-up-submit-button' onClick={onSubmitSignUpForm}>Sign Up</button>
+            </form>
+            {signUpFailed ? 'sign up failed' : null}
+        </div>
+    );
 }
 
 export default SignUpPage
