e3657d2b07c01d48fe46c075270f6b80d373be47
hanchen <hanchenjhc@gmail.com>
2019-09-27 10:03:20 -0500

    convert SearchBox and MenuButton into function
    [#168807411] (https://www.pivotaltracker.com/story/show/168807411)

--- src/SearchBar/MenuButton.js
+++ src/SearchBar/MenuButton.js
@@ -1,17 +1,15 @@
 import React from "react";
 import '../styles/MenuButton.scss';
 
-class MenuButton extends React.Component {
+const MenuButton = (props) => {
 
-    render() {
-        return (
-            <div className="menu-button-container" onClick={this.props.onClick}>
-                <div className="menu-button-bar"/>
-                <div className="menu-button-bar"/>
-                <div className="menu-button-bar"/>
-            </div>
-        );
-    }
+    return (
+        <div className="menu-button-container" onClick={props.onClick}>
+            <div className="menu-button-bar"/>
+            <div className="menu-button-bar"/>
+            <div className="menu-button-bar"/>
+        </div>
+    );
 }
 
 export default MenuButton;
