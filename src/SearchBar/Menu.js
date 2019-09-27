566118d07a35e9e102999cd1b9039aa3be915e9f
hanchen <hanchenjhc@gmail.com>
2019-09-27 09:58:22 -0500

    convert Menu into function
    [#168807411] (https://www.pivotaltracker.com/story/show/168807411)

--- src/SearchBar/Menu.js
+++ src/SearchBar/Menu.js
@@ -3,8 +3,8 @@ import '../styles/Menu.scss'
 import {Link} from "react-router-dom";
 import axios from "axios";
 
-class Menu extends React.Component {
-    onLogout = () => {
+const Menu = () => {
+    const onLogout = () => {
         console.log("logout clicked");
         axios.get(process.env.REACT_APP_SERVER_URL + '/users/logout', {withCredentials: true})
             .then(response => {
@@ -15,20 +15,18 @@ class Menu extends React.Component {
             })
     };
 
-    render() {
-        return (
-            <div className="menu-container">
-                <Link to="login" id='menu-login-option'>Log in</Link>
-                <Link to="sign-up" id='menu-signup-option'>Sign Up</Link>
-                <Link to="list-item">List Item</Link>
-                <div onClick={this.onLogout}>Logout</div>
-                <div>How it works</div>
-                <div>Trust & Safety</div>
-                <div>Help</div>
-                <div>Terms & Privacy</div>
-            </div>
-        );
-    }
-}
+    return (
+        <div className="menu-container">
+            <Link to="login" id='menu-login-option'>Log in</Link>
+            <Link to="sign-up" id='menu-signup-option'>Sign Up</Link>
+            <Link to="list-item">List Item</Link>
+            <div onClick={onLogout}>Logout</div>
+            <div>How it works</div>
+            <div>Trust & Safety</div>
+            <div>Help</div>
+            <div>Terms & Privacy</div>
+        </div>
+    );
+};
 
 export default Menu;
