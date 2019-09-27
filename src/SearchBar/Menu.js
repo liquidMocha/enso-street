3e0bc96cb0c9a6fc492d383f71db20a93c69ff8f
hanchen <hanchenjhc@gmail.com>
2019-09-26 21:09:00 -0500

    user can logout
    [#167964103] (https://www.pivotaltracker.com/story/show/167964103)

--- src/SearchBar/Menu.js
+++ src/SearchBar/Menu.js
@@ -1,14 +1,27 @@
 import React from "react";
 import '../styles/Menu.scss'
 import {Link} from "react-router-dom";
+import axios from "axios";
 
 class Menu extends React.Component {
+    onLogout = () => {
+        console.log("logout clicked");
+        axios.get(process.env.REACT_APP_SERVER_URL + '/users/logout', {withCredentials: true})
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    };
+
     render() {
         return (
             <div className="menu-container">
                 <Link to="login" id='menu-login-option'>Log in</Link>
                 <Link to="sign-up" id='menu-signup-option'>Sign Up</Link>
                 <Link to="list-item">List Item</Link>
+                <div onClick={this.onLogout}>Logout</div>
                 <div>How it works</div>
                 <div>Trust & Safety</div>
                 <div>Help</div>
