04eac74d309129fea4b5b1aa520376d0d4fed561
hanchen <hanchenjhc@gmail.com>
2019-10-03 20:47:10 -0500

    Add calendar field to homepage
    [#168937917] (https://www.pivotaltracker.com/story/show/168937917)

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,8 +1,19 @@
 import React from "react";
+import './styles/Button.scss';
+import './styles/Input.scss';
+import './styles/HomePage.scss';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 
 const HomePage = () => {
     return (
-        <div>
+        <div id='home-page'>
+            <div className='input' id='date-range-opener'>
+                <FontAwesomeIcon icon={faCalendarAlt}/>
+                <i className='far fa-calendar-alt'/>
+            </div>
+
+            <button>Search</button>
         </div>
     )
 };
--- src/MenuPage.js
+++ src/MenuPage.js
@@ -1,13 +1,13 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import './styles/HomePage.scss';
+import './styles/MenuPage.scss';
 
 const MenuPage = () => {
 
     return (
-        <div className='home-page'>
-            <div id='home-page-body'>
-                <div id='home-page-button-group'>
+        <div className='menu-page'>
+            <div id="menu-page-body">
+                <div id='menu-page-button-group'>
                     <Link to='login' id='login-button'>
                         <div className='home-page-button'>
                             Log in
@@ -21,7 +21,7 @@ const MenuPage = () => {
                 </div>
                 <div>How does it work?</div>
             </div>
-            <div id='home-page-policy-section'>
+            <div id='menu-page-policy-section'>
                 <div>Policies</div>
                 <div>Privacy Policy</div>
                 <div>Terms & Conditions</div>
