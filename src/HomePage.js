640eb4d2d99add76797ebbd333cbc1176190f06e
hanchen <hanchenjhc@gmail.com>
2019-10-04 10:55:02 -0500

    Add date picker modal
    [#168937917] (https://www.pivotaltracker.com/story/show/168937917)

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
+import Modal from 'react-modal';
 import './styles/Button.scss';
 import './styles/Input.scss';
 import './styles/HomePage.scss';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+import {faCalendarAlt} from '@fortawesome/free-solid-svg-icons';
 
 const HomePage = () => {
+    const [displayDatePicker, toggleDatePicker] = useState(false);
+
     return (
         <div id='home-page'>
-            <div className='input' id='date-range-opener'>
-                <FontAwesomeIcon icon={faCalendarAlt}/>
-                <i className='far fa-calendar-alt'/>
-            </div>
+            {displayDatePicker ? null :
+                <div>
+                    <div className='input'
+                         id='date-range-opener'
+                         onClick={() => {
+                             toggleDatePicker(true)
+                         }}>
+                        <FontAwesomeIcon icon={faCalendarAlt}/>
+                        <i className='far fa-calendar-alt'/>
+                    </div>
 
-            <button>Search</button>
+                    <button id='home-page-search-button'>Search</button>
+                </div>
+            }
+
+            <Modal className='date-range-picker-modal'
+                   overlayClassName="date-range-picker-overlay"
+                   isOpen={displayDatePicker}>
+                <div id='date-range-picker-title-bar'>
+                    <span />
+                    <span id='date-range-picker-title'>Select Dates</span>
+                    <span id='date-range-picker-close' onClick={() => {
+                        toggleDatePicker(false);
+                    }}>Done
+                    </span>
+                </div>
+            </Modal>
         </div>
     )
 };
--- src/nightwatch_tests/HomePageTest.js
+++ src/nightwatch_tests/HomePageTest.js
@@ -18,5 +18,25 @@ module.exports = {
             .click('.menu-button-container')
             .click('#enso-street-title')
             .expect.url().to.endWith(url + '/')
+    },
+
+    'should open date picker modal when click on date picker': (browser) => {
+        browser
+            .url(url)
+            .waitForElementVisible('body')
+            .click('#date-range-opener')
+            .assert.visible('.date-range-picker-modal')
+            .assert.elementNotPresent('#home-page-search-button')
+            .end();
+    },
+
+    'should close date picker modal when click done': (browser) => {
+        browser.url(url)
+            .waitForElementVisible('body')
+            .click('#date-range-opener')
+            .click('#date-range-picker-close')
+            .assert.elementNotPresent('.date-range-picker-modal')
+            .assert.visible('#home-page-search-button')
+            .end();
     }
 };
