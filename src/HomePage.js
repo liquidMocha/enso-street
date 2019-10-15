10d592d3b1d50317566ec1b44979fe6745048331
hanchen <hanchenjhc@gmail.com>
2019-10-14 22:06:35 -0500

    remove location permission popup from homepage
    [#169115001] (https://www.pivotaltracker.com/story/show/169115001)

--- src/HomePage.js
+++ src/HomePage.js
@@ -7,38 +7,14 @@ import {faCalendarAlt, faMapMarkerAlt} from '@fortawesome/free-solid-svg-icons';
 import DateRangePickerModal from "./DateRangePickerModal";
 import LocationPickerModal from "./LocationPickerModal";
 import './styles/Sizing.scss';
-import Modal from "react-modal";
 
 const HomePage = () => {
     const [displayDatePicker, toggleDatePicker] = useState(false);
     const [displayLocationPicker, toggleLocationPicker] = useState(false);
     const [rentDate, setRentDate] = useState(new Date());
     const [returnDate, setReturnDate] = useState(new Date());
-    const [displayLocationPermission, toggleLocationPermissionPopup] = useState(true);
-    const [hasAskedLocationPermission, setHasAskedLocationPermission] = useState(false);
 
     const dateFormatter = new Intl.DateTimeFormat('en-US', {month: 'short', day: '2-digit'});
-
-    const popupModalStyles = {
-        content: {
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            fontSize: '16px',
-            height: '90px',
-            width: '50vw',
-            borderRadius: 0
-        }
-    };
-
-    window.navigator.permissions.query({name: 'geolocation'})
-        .then((result) => {
-            if (result.state === 'granted' || result.state === 'denied') {
-                setHasAskedLocationPermission(true);
-            } else {
-                setHasAskedLocationPermission(false);
-            }
-        });
 
     return (
         <div>
@@ -62,23 +38,6 @@ const HomePage = () => {
                     <button id='home-page-search-button'>Search</button>
                 </div>
             }
-            <Modal id='location-permission-modal'
-                   isOpen={displayLocationPermission && !hasAskedLocationPermission}
-                   overlayClassName="popup-modal-overlay"
-                   style={popupModalStyles}>
-                "Enso Street" would like to user your current location?
-                <div className='horizontal-layout button-group'>
-                    <div id='do-not-allow-location-button' onClick={() => {
-                        toggleLocationPermissionPopup(false);
-                        toggleLocationPicker(true);
-                    }}>Don't allow
-                    </div>
-                    <div id='allow-location-button' onClick={() => {
-                        toggleLocationPermissionPopup(false);
-                    }}>Ok
-                    </div>
-                </div>
-            </Modal>
             <DateRangePickerModal displayDatePicker={displayDatePicker}
                                   toggleDatePicker={toggleDatePicker}
                                   setRentDate={setRentDate}
