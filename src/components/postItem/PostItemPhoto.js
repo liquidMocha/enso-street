221dc8549067e5290eb9ef9a01a513b0d8e6ebb8
hanchen <hanchenjhc@gmail.com>
2019-11-07 21:30:51 -0600

    Add select photo button to post item photo page

--- src/components/postItem/PostItemPhoto.js
+++ src/components/postItem/PostItemPhoto.js
@@ -5,6 +5,7 @@ import '../../styles/Image.scss';
 import '../../styles/Input.scss';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCamera} from "@fortawesome/free-solid-svg-icons";
+import {faImages} from "@fortawesome/free-solid-svg-icons";
 
 const PostItemPhoto = (props) => {
     const [imageUrl, setImageUrl] = useState(null);
@@ -12,21 +13,35 @@ const PostItemPhoto = (props) => {
     return (
         <div>
             <PostItemTitleBar/>
-            {imageUrl ? <img src={imageUrl}/> : null}
+            {imageUrl ? <img src={imageUrl} alt='User selected item'/> : null}
             <div id='title-row'>Title: {props.postedItemTitle}</div>
             {
                 imageUrl ? null :
-                    <div className='image-button'>
-                        <input id='take-photo-input' type="file" accept="image/*" capture="camera"
-                               onChange={(event) => {
-                                   if (event.target.files && event.target.files[0]) {
-                                       setImageUrl(URL.createObjectURL(event.target.files[0]));
-                                   }
-                               }}/>
-                        <label htmlFor='take-photo-input'>
-                            <FontAwesomeIcon icon={faCamera}/>
-                            Take Photo
-                        </label>
+                    <div className='column-layout'>
+                        <div className='image-button'>
+                            <input id='take-photo-input' type="file" accept="image/*" capture="camera"
+                                   onChange={(event) => {
+                                       if (event.target.files && event.target.files[0]) {
+                                           setImageUrl(URL.createObjectURL(event.target.files[0]));
+                                       }
+                                   }}/>
+                            <label htmlFor='take-photo-input'>
+                                <FontAwesomeIcon icon={faCamera}/>
+                                Take Photo
+                            </label>
+                        </div>
+                        <div className='image-button'>
+                            <input id='select-image-input' type="file" accept="image/*"
+                                   onChange={(event) => {
+                                       if (event.target.files && event.target.files[0]) {
+                                           setImageUrl(URL.createObjectURL(event.target.files[0]));
+                                       }
+                                   }}/>
+                            <label htmlFor='take-photo-input'>
+                                <FontAwesomeIcon icon={faImages}/>
+                                Select Photo
+                            </label>
+                        </div>
                     </div>
             }
         </div>
