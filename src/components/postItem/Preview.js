7ce18bf12f5f7a67fbc376f3e02b067533766fb0
hanchen <hanchenjhc@gmail.com>
2019-11-19 21:14:47 -0600

    Styling Preview page + add edit and post button

--- src/components/postItem/Preview.js
+++ src/components/postItem/Preview.js
@@ -2,8 +2,10 @@ import React from "react";
 import PostItemTitleBar from "../shared/PostItemTitleBar";
 import {connect} from "react-redux";
 import "../../styles/Image.scss";
+import "../../styles/Preview.scss";
+import {withRouter} from "react-router-dom";
 
-const Preview = (props) => {
+const Preview = withRouter((props) => {
 
     const renderCategories = () => {
         return props.categories.map(category => {
@@ -11,39 +13,38 @@ const Preview = (props) => {
         })
     };
 
+    const onClickingEdit = () => {
+        props.history.push('/post-item/price-and-delivery');
+    };
+
     return (
-        <div>
+        <div className='column-layout left-aligned'>
             <PostItemTitleBar backLink="/post-item/price-and-delivery"/>
             <img src={props.imageUrl} alt="Posted Item"/>
             <div className='bold'>{props.itemTitle}</div>
-            <div className='horizontal-layout'>
-                <span>{props.dailyPrice} per day</span>
-                <span>{props.deposit} deposit</span>
+            <div className='horizontal-layout' id='preview-prices'>
+                <span>${props.dailyPrice} per day</span>
+                <span>${props.deposit} deposit</span>
             </div>
             <div>
-                <span className='bold'>Categories</span>
+                <span className='bold key-value-column'>Categories</span>
                 {renderCategories()}
             </div>
             <div>
-                <span className='bold'>Condition</span>
+                <span className='bold key-value-column'>Condition</span>
                 {props.condition.label}
             </div>
             <div>
                 <span className='bold'>Description</span>
                 {props.description}
             </div>
-            <div>
-                <span className='bold'>Item size</span>
-                {props.itemSize.label}</div>
-            {
-                props.canBeDelivered ?
-                    <div>
-                        Item can be delivered
-                    </div> : null
-            }
+            <div className='horizontal-layout' id='preview-buttons'>
+                <button className='preview-button' onClick={onClickingEdit}>Edit</button>
+                <button className='preview-button'>Post</button>
+            </div>
         </div>
     )
-};
+});
 
 const mapStateToProps = (state) => {
     return {
