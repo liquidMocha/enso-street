1f33c023554aebdfa644bd361c0a6dcd67c49dcc
hanchen <hanchenjhc@gmail.com>
2019-11-30 10:50:23 -0600

    No longer forcing image to be 100% width of parent
    [#170033636] (https://www.pivotaltracker.com/story/show/170033636)

--- src/components/postItem/PostItemPage.js
+++ src/components/postItem/PostItemPage.js
@@ -1,15 +1,16 @@
 import React from "react";
-import '../../styles/Input.scss';
 import PostItemTitleBar from "../shared/PostItemTitleBar";
 import {connect} from "react-redux";
-import '../../styles/Button.scss';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faImage} from "@fortawesome/free-solid-svg-icons";
 import NextButton from "./NextButton";
 import {updatePostedItemImageUrl, updatePostedItemTitle} from "../../redux/postItemActions";
 import InputWithError from "../shared/InputWithError";
 import {Link} from "react-router-dom";
+import "../../styles/Input.scss";
+import "../../styles/Button.scss";
 import "../../styles/App.scss";
+import "../../styles/Image.scss";
 
 export const PostItemPage = (props) => {
     return (
