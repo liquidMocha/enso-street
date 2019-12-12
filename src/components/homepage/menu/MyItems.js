a43da37cce787b18c988085f77a66e01b298d9b1
hanchen <hanchenjhc@gmail.com>
2019-12-11 22:00:15 -0600

    Filter My Items by title, description and address as user types in search input
    [#170243292] (https://www.pivotaltracker.com/story/show/170243292)

--- src/components/homepage/menu/MyItems.js
+++ src/components/homepage/menu/MyItems.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {deleteItem, getAllItemsForUser} from "../../../services/ItemService";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faAngleLeft} from "@fortawesome/free-solid-svg-icons";
+import {faAngleLeft, faSearch} from "@fortawesome/free-solid-svg-icons";
 import DollarInput from "../../shared/DollarInput";
 import {faSave, faTrashAlt} from "@fortawesome/free-regular-svg-icons";
 import {useHistory} from "react-router-dom";
@@ -10,23 +10,40 @@ import "../../../styles/MyItem.scss";
 
 const MyItems = () => {
     const [items, setItems] = useState([]);
+    const [visibleItems, setVisibleItems] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
     let history = useHistory();
 
     useEffect(() => {
         getAllItemsForUser()
             .then(result => {
-                setItems(result.data.sort((a, b) => {
-                    const timeA = a.createdOn;
-                    const timeB = b.createdOn;
-                    if (timeA > timeB) {
-                        return -1;
-                    } else if (timeA < timeB) {
-                        return 1;
-                    }
-                    return 0;
-                }));
+                setItems(result.data.sort(byCreatedOn));
+                setVisibleItems(result.data.sort(byCreatedOn));
             });
     }, []);
+
+    useEffect(() => {
+        if (searchTerm.length !== 0) {
+            setVisibleItems(items.filter(item => {
+                return item.title.includes(searchTerm) ||
+                    (item.description && item.description.includes(searchTerm)) ||
+                    (item.address && item.address.includes(searchTerm));
+            }))
+        } else {
+            setVisibleItems(items);
+        }
+    }, [searchTerm]);
+
+    const byCreatedOn = (a, b) => {
+        const timeA = a.createdOn;
+        const timeB = b.createdOn;
+        if (timeA > timeB) {
+            return -1;
+        } else if (timeA < timeB) {
+            return 1;
+        }
+        return 0;
+    };
 
     return (
         <div>
@@ -39,7 +56,11 @@ const MyItems = () => {
                 <span className='fixed-title-bar__title--font'>My Items</span>
                 <span/>
             </div>
-            {items.map(item => {
+            <input type='text' placeholder='Search my item' onChange={(event => {
+                setSearchTerm(event.target.value);
+            })}/>
+            <FontAwesomeIcon icon={faSearch}/>
+            {visibleItems.map(item => {
                 return (
                     <div key={item.id} className='my-item-card'>
                         <div className='column-layout my-item-card-content'>
