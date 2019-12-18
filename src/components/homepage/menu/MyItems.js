d6c20b527333580213e6208a6c9d222b7234fabc
hanchen <hanchenjhc@gmail.com>
2019-12-18 17:12:00 -0600

    Add confirmation modal when clicking delete button on My Items page.
    [#170344151] (https://www.pivotaltracker.com/story/show/170344151)

--- src/components/homepage/menu/MyItems.js
+++ src/components/homepage/menu/MyItems.js
@@ -5,6 +5,7 @@ import {faAngleLeft, faSearch} from "@fortawesome/free-solid-svg-icons";
 import DollarInput from "../../shared/DollarInput";
 import {faSave, faTrashAlt} from "@fortawesome/free-regular-svg-icons";
 import {useHistory} from "react-router-dom";
+import Modal from 'react-modal';
 import "../../../styles/Spacing.scss";
 import "../../../styles/MyItem.scss";
 import "../../../styles/Input.scss";
@@ -13,6 +14,10 @@ const MyItems = () => {
     const [items, setItems] = useState([]);
     const [visibleItems, setVisibleItems] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
+    const [deleteModalStatus, setDeleteModalStatus] = useState({
+        isOpen: false, itemId: ''
+    });
+
     let history = useHistory();
 
     useEffect(() => {
@@ -26,23 +31,26 @@ const MyItems = () => {
 
     useEffect(() => {
         if (searchTerm.length !== 0) {
-            setVisibleItems(
-                items.filter(item => {
-                    return item.title.toLowerCase().includes(searchTerm) ||
-                        (item.description && item.description.toLowerCase().includes(searchTerm)) ||
-                        (item.location &&
-                            (
-                                (item.location.street && item.location.street.toLowerCase().includes(searchTerm)) ||
-                                (item.location.zipCode && item.location.zipCode.toLowerCase().includes(searchTerm)) ||
-                                (item.location.city && item.location.city.toLowerCase().includes(searchTerm)) ||
-                                (item.location.state && item.location.state.toLowerCase().includes(searchTerm)) ||
-                                (item.location.nickname && item.location.nickname.toLowerCase().includes(searchTerm))
-                            ));
-                }))
+            setVisibleItems(filterItemBySearchTerm(items))
         } else {
             setVisibleItems(items);
         }
     }, [searchTerm, items]);
+
+    const filterItemBySearchTerm = (items) => {
+        return items.filter(item => {
+            return item.title.toLowerCase().includes(searchTerm) ||
+                (item.description && item.description.toLowerCase().includes(searchTerm)) ||
+                (item.location &&
+                    (
+                        (item.location.street && item.location.street.toLowerCase().includes(searchTerm)) ||
+                        (item.location.zipCode && item.location.zipCode.toLowerCase().includes(searchTerm)) ||
+                        (item.location.city && item.location.city.toLowerCase().includes(searchTerm)) ||
+                        (item.location.state && item.location.state.toLowerCase().includes(searchTerm)) ||
+                        (item.location.nickname && item.location.nickname.toLowerCase().includes(searchTerm))
+                    ));
+        })
+    };
 
     const byCreatedOn = (a, b) => {
         const timeA = a.createdOn;
@@ -53,6 +61,15 @@ const MyItems = () => {
             return 1;
         }
         return 0;
+    };
+
+    const deleteItemById = (itemId) => {
+        deleteItem(itemId).then(() => {
+            const itemsLeft = items.filter(existingItem => {
+                return existingItem.id !== itemId
+            });
+            setItems(itemsLeft);
+        })
     };
 
     return (
@@ -97,18 +114,29 @@ const MyItems = () => {
                         </div>
                         <div className='my-item-card-buttons column-layout'>
                             <FontAwesomeIcon icon={faTrashAlt} onClick={() => {
-                                deleteItem(item.id).then(() => {
-                                    const itemsLeft = items.filter(existingItem => {
-                                        return existingItem.id !== item.id
-                                    });
-                                    setItems(itemsLeft);
-                                })
+                                setDeleteModalStatus({isOpen: true, itemId: item.id});
                             }}/>
                             <FontAwesomeIcon icon={faSave}/>
                         </div>
                     </div>
                 )
             })}
+            <Modal isOpen={deleteModalStatus.isOpen}
+                   className='delete-item-modal'
+                   overlayClassName="delete-item-modal-overlay">
+                <div>Are you sure you want to delete this item?</div>
+                <div className='modal-button-group'>
+                    <div onClick={() => {
+                        deleteItemById(deleteModalStatus.itemId);
+                        setDeleteModalStatus({isOpen: false, itemId: ''});
+                    }}>Delete
+                    </div>
+                    <div onClick={() => {
+                        setDeleteModalStatus({isOpen: false, itemId: ''})
+                    }}>Cancel
+                    </div>
+                </div>
+            </Modal>
         </div>
     )
 };
