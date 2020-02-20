62cb07ccf945ffd19c670103f0f0046f77cf88af
hanchen <hanchenjhc@gmail.com>
2020-02-19 18:24:13 -0600

    setup redux dev tool

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import thunkMiddleware from 'redux-thunk';
 import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import combinedReducers from './reducers/combinedReducers';
 
-export default createStore(combinedReducers, applyMiddleware(thunkMiddleware));
+export default createStore(combinedReducers, composeWithDevTools(applyMiddleware(thunkMiddleware)));
