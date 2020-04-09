dc9848db6c39252b8bcbd133ec1ea54e8348b017
hanchen <hanchenjhc@gmail.com>
2020-04-08 20:51:17 -0500

    Add checkbox for I'd like to pickup option in checkout

--- src/components/cart/checkout/DeliveryOrPickupSection.jsx
+++ src/components/cart/checkout/DeliveryOrPickupSection.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Checkbox from '../../shared/Checkbox';
 
-const DeliveryOrPickupSection = () => (
-  <div>
-    I'd like to pickup
-  </div>
-);
+const DeliveryOrPickupSection = () => {
+  const [pickup, setPickup] = useState(false);
+
+  return (
+    <div>
+      <Checkbox onChange={() => setPickup(!pickup)} checked={pickup} />
+      I'd like to pickup
+    </div>
+  );
+};
 
 export default DeliveryOrPickupSection;
