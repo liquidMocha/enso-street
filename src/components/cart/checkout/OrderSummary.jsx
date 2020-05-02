d98e730ae7c4b631aee5c1ec2c70d898bf5d3109
hanchen <hanchenjhc@gmail.com>
2020-05-02 13:30:28 -0500

    Styling order summary

--- src/components/cart/checkout/OrderSummary.jsx
+++ src/components/cart/checkout/OrderSummary.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './OrderSummary.scss';
 
 const OrderSummary = ({ subtotal, deliveryFee, calculatingDeliveryFee }) => (
-  <section>
-    <label>Subtotal: </label>
-    <span>
-      {`$${subtotal}`}
-    </span>
-    <label>Delivery: </label>
-    <span>
-      {calculatingDeliveryFee ? 'calculating...' : `$${deliveryFee}`}
-    </span>
+  <section className="checkout__order-summary">
+    <section>
+      <label>Subtotal: </label>
+      <span>
+        {`$${subtotal}`}
+      </span>
+    </section>
+    <section>
+      <label>Delivery: </label>
+      <span>
+        {calculatingDeliveryFee ? 'calculating...' : `$${deliveryFee}`}
+      </span>
+    </section>
+    <section>
+      <label className="checkout__order-summary--emphasis">Order total: </label>
+      <span>{calculatingDeliveryFee ? 'calculating...' : `$${subtotal + deliveryFee}`}</span>
+    </section>
   </section>
 );
 
