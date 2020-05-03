ba4e66a71de2ed9cbe5c8cb95837ab0d4f6231b2
hanchen <hanchenjhc@gmail.com>
2020-05-03 15:09:39 -0500

    Pass start time and return time to backend when confirming an order

--- src/services/TransactionService.js
+++ src/services/TransactionService.js
@@ -6,15 +6,13 @@ const transactionBasePath = '/transaction';
 export const getDeliveryPrice = async (itemIds, deliveryAddress) => (await axios.post(`${BASE_URL}${transactionBasePath}/delivery-quote`,
   { itemIds, deliveryAddress })).data;
 
-export const createPaymentIntent = async (items, needsDelivery, deliveryAddress, rentalDays) => {
-  return (await axios.post(`${BASE_URL}${transactionBasePath}/pay`, {
-    items: items.map((item) => ({
-      id: item.id,
-      quantity: item.quantity,
-    })),
-    needsDelivery,
-    deliveryAddress,
-    rentalDays,
-  }, { withCredentials: true }))
-    .data.clientSecret;
-};
+export const createPaymentIntent = async (items, needsDelivery, deliveryAddress, rentDate, returnDate) => (await axios.post(`${BASE_URL}${transactionBasePath}/pay`, {
+  items: items.map((item) => ({
+    id: item.id,
+    quantity: item.quantity,
+  })),
+  needsDelivery,
+  deliveryAddress,
+  rentDate,
+  returnDate,
+}, { withCredentials: true })).data.clientSecret;
