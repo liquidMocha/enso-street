aff074cb1ec1ad9cdab98b8a2d0bbce5ab2699ff
hanchen <hanchenjhc@gmail.com>
2021-03-16 21:16:56 -0500

    Fix bug where cart icon disappears from title bar
    + adjust title bar links to be actual links so they can be tabbed on

--- src/components/shared/TitleBar.jsx
+++ src/components/shared/TitleBar.jsx
@@ -1,36 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import './TitleBar.scss';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { faBars, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useSelector } from 'react-redux';
 import { getUserProfile } from '../../services/UserProfileService';
 
-function navigateToCart(history) {
-  history.push('/my-cart');
-}
-
 function cartWithCount(history, count) {
   return (
-    <span
+    <Link
+      to="/my-cart"
       className="fa-layers fa-fw"
-      onClick={() => {
-        navigateToCart(history);
-      }}
+      id="title-bar-cart-icon"
     >
+      <FontAwesomeIcon icon={faShoppingCart} />
       {count !== 0
         && (
-          <>
-            <FontAwesomeIcon icon={faShoppingCart} />
-            <span
-              className="fa-layers-counter fa-layers-top-right fa-lg"
-              style={{ background: 'Tomato' }}
-            >
-              {count}
-            </span>
-          </>
+        <span
+          className="fa-layers-counter fa-layers-top-right fa-lg"
+          style={{ background: 'Tomato' }}
+        >
+          {count}
+        </span>
         )}
-    </span>
+    </Link>
   );
 }
 
@@ -46,14 +39,9 @@ const TitleBar = () => {
 
   return (
     <div className="fixed-title-bar">
-      <h1
-        id="enso-street-title"
-        onClick={() => {
-          history.push('/');
-        }}
-      >
+      <Link to="/" id="enso-street-title">
         Enso Street
-      </h1>
+      </Link>
       <div id="title-bar__right-section">
         <span className="title-bar__greetings">
           Hello
